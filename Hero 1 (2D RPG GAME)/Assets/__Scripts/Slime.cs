1f7f7a6477e285b0909fa88c576ed6366bbdb6bb
Omar Benhmuda <omar2kb@gmail.com>
2020-04-03 22:34:34 -0400

    Added RPG Game from Software Construction

---
+++ Hero 1 (2D RPG GAME)/Assets/__Scripts/Slime.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.UIElements.GraphView;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using playerClass;
+
+public class Slime : Enemy1
+{
+   
+
+    
+    
+    private void Update()
+    {
+        if (enemy1Health <= 0)
+        {
+            Destroy(enemy1);
+            Player.TeleportToLevel2();
+            
+        }
+
+        Vector2 slimeScale = transform.localScale;
+
+
+        switch (direction)
+        {
+            //SLIME MOVES RIGHT IF DIRECTION IS RIGHT
+            case 'R':
+                transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+                slimeScale.x = 0.5f;
+                break;
+            //SLIME MOVES LEFT IF DIRECTION IS LEFT
+            case 'L':
+                transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+                slimeScale.x = -0.5f;
+                break;
+        }
+
+        transform.localScale = slimeScale;
+    }
+}
